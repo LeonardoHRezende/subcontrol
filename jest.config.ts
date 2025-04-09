041e81f9b77cc5cd6054630aa0e96615c1ce0db0
LeonardoHRezende <rezendeleo64@gmail.com>
2025-04-09 18:56:05 -0300

    feat: setup tests

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+};
+
+export default config;
