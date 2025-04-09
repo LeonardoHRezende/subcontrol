92de141e858dd3bbd16abaf5ea47e643967ef801
LeonardoHRezende <rezendeleo64@gmail.com>
2025-04-09 18:58:37 -0300

    feat: improve container

--- src/components/templates/container.tsx
+++ src/components/templates/container.tsx
@@ -1,17 +1,14 @@
-import * as React from "react";
+import type * as React from "react"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 function Container({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      className={cn(
-        "flex items-center justify-center min-h-screen bg-zinc-50 dark:bg-zinc-950",
-        className
-      )}
+      className={cn("min-h-screen mx-auto bg-zinc-50 dark:bg-zinc-950 p-4 grid place-items-center", className)}
       {...props}
     />
-  );
+  )
 }
 
-export { Container };
+export { Container }
